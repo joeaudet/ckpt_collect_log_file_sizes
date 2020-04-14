4cf1b1df51d8b925877b08c77367208f16566a4c
joeaudet <jaudet@ans-llc.net>
2020-04-14 13:09:07 -0400

    Convert tabs to spaces

--- collect_log_file_sizes.sh
+++ collect_log_file_sizes.sh
@@ -28,35 +28,35 @@ OUTPUT_FILE_NAME="$OUTPUT_DIR/$HOSTNAME-log_sizing_output-$TODAY.txt";
 #Define a function to generate the output of file sizes we want
 collect_log_file_data()
 {
-        LOG_FILE_SIZE="";
-        LOG_FILE_NAME="";
-		LOG_FILE_CUSTOMER="";
-        LOG_FILE_SIZE="$(ls -l $FWDIR/log/$(date --date="-$x days" +"%Y-%m-%d")*.log | awk '{print $5}')";
-        LOG_FILE_NAME="$(ls -l $FWDIR/log/$(date --date="-$x days" +"%Y-%m-%d")*.log | awk '{print $9}')";
-        LOG_FILE_CUSTOMER="$(dirname $LOG_FILE_NAME | cut -d '/' -f5)";
-        echo "$LOG_FILE_SIZE,$LOG_FILE_CUSTOMER,$(basename $LOG_FILE_NAME)" >> $OUTPUT_FILE_NAME;
+    LOG_FILE_SIZE="";
+    LOG_FILE_NAME="";
+    LOG_FILE_CUSTOMER="";
+    LOG_FILE_SIZE="$(ls -l $FWDIR/log/$(date --date="-$x days" +"%Y-%m-%d")*.log | awk '{print $5}')";
+    LOG_FILE_NAME="$(ls -l $FWDIR/log/$(date --date="-$x days" +"%Y-%m-%d")*.log | awk '{print $9}')";
+    LOG_FILE_CUSTOMER="$(dirname $LOG_FILE_NAME | cut -d '/' -f5)";
+    echo "$LOG_FILE_SIZE,$LOG_FILE_CUSTOMER,$(basename $LOG_FILE_NAME)" >> $OUTPUT_FILE_NAME;
 }
 
 # Process differently for an SMS versus an MDS server
 if [ -z ${MDSVERUTIL+x} ];
 then
-                #Go back specified amount of days and collect log data
-                for (( x=DAYS_BACK; x>=1; --x ))
-                do
-                    collect_log_file_data
-                done
+    #Go back specified amount of days and collect log data
+    for (( x=DAYS_BACK; x>=1; --x ))
+    do
+        collect_log_file_data
+    done
 else
-                #Loop through each MDS domain on the server
-                for CMA_NAME in $($MDSVERUTIL AllCMAs)
-				do
-					echo "Currently processing domain: $CMA_NAME";
-					mdsenv $CMA_NAME
-					#Go back specified amount of days and collect log data
-					for (( x=DAYS_BACK; x>=1; --x ))
-					do
-							collect_log_file_data
-					done
-                done
+    #Loop through each MDS domain on the server
+    for CMA_NAME in $($MDSVERUTIL AllCMAs)
+    do
+        echo "Currently processing domain: $CMA_NAME";
+        mdsenv $CMA_NAME
+        #Go back specified amount of days and collect log data
+        for (( x=DAYS_BACK; x>=1; --x ))
+        do
+                collect_log_file_data
+        done
+    done
 fi
 
 echo "Logged output stored in the following file: $OUTPUT_FILE_NAME"
